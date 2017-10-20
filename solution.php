5c3d05dd78dee9c998a7813774343bde709fd38f
Devontrae Walls <devontrae@forklabscreative.com>
2017-10-20 16:02:07 -0700

    Filled out parse_request function. It takes a request, and converts given request into a decodeable version where it then splits the string into the signature and payload parts; A request_signature and a request_payload, it then creates a new signature from the request payload, and checks if it matches the given request_payload. If it matches, it returns the decoded request_payload, otherwise it returns false. Assertion tests 1-3 passed.

--- solution.php
+++ solution.php
@@ -1,9 +1,49 @@
 <?php
 // YOUR NAME AND EMAIL GO HERE
+// Devontrae Walls / devontrae@gmail.com
 
+  /*function make_request($payload, $secret)
+  {
+      $payload   = json_encode($payload);
+      $signature = hash_hmac('sha256', $payload, $secret);
+      $request   = base64_encode($signature).'.'.base64_encode($payload);
+
+      return strtr($request, '+/', '-_');
+  }
+
+  for ($i = 0; $i < $iterations; $i++) {
+      $payload = ["s" => "string ".$i, "b" => (bool)($i % 2), "i" => $i, "f" => $i / 10];
+      $request = make_request($payload, API_SECRET);
+
+      assert(parse_request($request, API_SECRET) === $payload); // original
+      assert(parse_request(strrev($request), API_SECRET) === false); // reverse
+      assert(parse_request(substr($request, 1, -1), API_SECRET) === false); // shortened
+  }
+*/
 function parse_request($request, $secret)
 {
     // YOUR CODE GOES HERE
+    # The goal here is to return a new signature
+    # We need to decode the request string, first lets put our '+' back.
+    $request = strtr($request, '-_', '+/');
+    # Now lets separate the request
+    $parts = explode('.', $request);
+    $req_signature = base64_decode($parts[0]);
+    $req_payload = base64_decode($parts[1]); # This one is JSON now
+
+    # Now lets write a new signature given the secret supplied in the parse request, to make sure it matches
+    $signature = hash_hmac('sha256', $req_payload, $secret);
+
+    if($signature == $req_signature) {
+      # Great, our request signature and our new signature given the secret given match!
+      # Lets return the payload
+      echo "signatures match!\n";
+      return json_decode($req_payload, true);
+    } else {
+      echo "signatures do not match!\n";
+      return false;
+    }
+
 }
 
 function dates_with_at_least_n_scores($pdo, $n)
--- test.php
+++ test.php
@@ -15,7 +15,6 @@ assert(function_exists("dates_with_at_least_n_scores"));
 assert(function_exists("users_with_top_score_on_date"));
 assert(function_exists("dates_when_user_was_in_top_n"));
 
-// check encode/decode of the same payload
 for ($i = 0; $i < 1000; $i++) {
     $payload = ["s" => "string ".$i, "b" => (bool)($i % 2), "i" => $i, "f" => $i / 10];
     $request = make_request($payload, API_SECRET);
@@ -26,10 +25,10 @@ for ($i = 0; $i < 1000; $i++) {
 }
 
 
-$pdo = new PDO("sqlite:challenge.sqlite");
+$pdo = new PDO("sqlite:challenge.sqlite"); # Create a new database object
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-create_db_table($pdo);
+create_db_table($pdo); # reate the "challenge" table
 populate_db_from_csv($pdo, "./data/data-1.csv");
 
 $correct = ["2017-09-02", "2017-09-01", "2017-08-31"];
